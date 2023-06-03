427d88f1da29545576e635876565b1fed598270b
Jens J <af5mi@yahoo.com>
2023-06-03 17:53:06 -0500

    reworked the listen mode

--- README.md
+++ README.md
@@ -26,13 +26,25 @@ One complex idea with a single radio would be to allow double pairing, and have 
 ## pairing process
 pressing and holding button on the back of the remote will cause remote to begin rolling
 sequentially through all channels, and transmitting pairing offer to pipe addr `0x9669966994`
-(appears that some remotes roll through additional pipe sub-addresses prefixed w/ `0x96699669`, but 0x94 seemed to be common to the remotes I have)
+(appears that some remotes roll through additional pipe sub-addresses prefixed w/ `0x96699669`,
+e.g. 0x90, 0x91, 0x95 but 0x94 seemed to be common to the remotes I have)
 
 once the pairing sequence packets are all ack'ed by a receiver, remote will switch to this
 new address and channel and transmit there, until re-paired
 
 pairing light flashes while searching, is steady while going through pairing sequence, and 
 then will turn off when pairing is completed. 
+
+remotes already paired with a base may just re-pair on same channel & address if base is still listening,
+and may not try to pair on the pairing address.
+also I have seen some remotes which periodically do this re-pairing sequence (with already paired base),
+when just short-pressing the pair button. Perhaps this might be some quick re-pairing sequence, 
+e.g. in case of interference on same channel, etc.
+
+## listen mode
+I added a special listen mode which will listen for, and pair with a remote
+but then will itself try to emulate a remote and pair with a base
+using same channel & address, then just eavesdrop on the conversation
 
 ## buttons
 mystery: on some remotes these seem to have two-way feedback, e.g. fan & vibrator buttons cycle through modes/levels, and an indicator lights up and cycles on the remote as well (at least when using a real base receiver). I suspect there is either some ack payload, or perhaps some response on a nearby pipe (base address) a real base receiver transmits on to update remote about bed's state.
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 */
 
 #include <SPI.h>
-#include "nRF24L01.h"
+//#include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 
@@ -37,8 +37,18 @@
 #define   MT_PAIR_INIT  0x04
 #define   MT_PAIR_DONE  0x05
 
-// don't ack packets, just eavesdrop
-//#define LISTEN_ONLY_MODE 1 
+/*
+ in listen only mode, we need to emulate a base
+ so a remote can pair to us, and tell us which channel & pipe addr to listen on
+ then we can emulate a remote, & try to pair with a base receiver,
+ to move him to listen to the real remote
+ then we sit back and listen in
+
+ with listen_mode = false, we just emulate a base to pair with a remote
+*/
+bool listen_mode = false;
+
+#define MAX_CHANNELS  82
 
 // Set up nRF24L01 radio on SPI bus plus pins 7 & 8
 RF24 radio(7,8);
@@ -47,17 +57,22 @@ uint8_t buf[32];
 uint8_t msgbuf[32];
 uint8_t pipenum;
 
-uint8_t newpipe[5] = {0,0,0,0,0};
+const uint8_t pairaddr[5] = {0x94, 0x69, 0x96, 0x69, 0x96};
+uint8_t pipeaddr[5] = {0x12, 0x34, 0x56, 0x78, 0x90};
+uint8_t pipeaddr_offset;
 
-bool pairing_started = false;
+bool remote_pair_started = false;
+bool remote_pair_done = false;
+bool base_pair_done = false;
+
 uint8_t pairing_ctr;
-
-uint8_t chan = 1; // initial listen channel
+uint32_t ts = 0;
+uint8_t chan = 1; // initial channel
 
 const uint8_t addresses[][5] = {
-  {0x90, 0x69, 0x96, 0x69, 0x96},
-  {0x91, 0x69, 0x96, 0x69, 0x96},
-  {0x92, 0x69, 0x96, 0x69, 0x96},
+  {0x94, 0x69, 0x96, 0x69, 0x96},
+  {0x00, 0x69, 0x96, 0x69, 0x96},
+  {0x96, 0x69, 0x96, 0x69, 0x96},
   {0x93, 0x69, 0x96, 0x69, 0x96},
   {0x94, 0x69, 0x96, 0x69, 0x96},
   {0x95, 0x69, 0x96, 0x69, 0x96},     
@@ -76,94 +91,182 @@ void setup(void)
 
   radio.begin();
   radio.setDataRate(RF24_1MBPS);
-  
-  #ifdef LISTEN_ONLY_MODE
-  radio.setAutoAck(false);
-  #else
   radio.setAutoAck(true);
-  #endif
   radio.setCRCLength(RF24_CRC_16);
   radio.enableDynamicPayloads(); // this might need to be flipped for clone radios?
   radio.stopListening();
-  radio.openReadingPipe(0, addresses[0]);
-  radio.openReadingPipe(1, addresses[1]);
-  radio.openReadingPipe(2, addresses[2]);
-  radio.openReadingPipe(3, addresses[3]);
-  radio.openReadingPipe(4, addresses[4]);
-  radio.openReadingPipe(5, addresses[5]);  
+  radio.openReadingPipe(0, pairaddr);
+  //radio.openReadingPipe(1, addresses[1]);
+  //radio.openReadingPipe(2, addresses[2]);
+  //radio.openReadingPipe(3, addresses[3]);
+  //radio.openReadingPipe(4, addresses[4]);
+  //radio.openReadingPipe(5, addresses[5]);  
 
   radio.setChannel(chan);
   radio.setPALevel(RF24_PA_LOW);
   radio.startListening();
   radio.printPrettyDetails();
+  printf("listen_mode: %d\n", listen_mode);
 }
 
 void loop(void)
 {
+  // regular listen mode
   if (radio.available(&pipenum))
-    {
-      // read msg header
-      radio.read(&buf, radio.getDynamicPayloadSize());
-      uint8_t msgsize = buf[0];
-      uint8_t msgcounter = buf[1];
-      uint8_t msgtype = buf[2];
-      
-      // read msg body
-      memcpy(msgbuf, &buf[3], msgsize);
+  {
+    // read msg header
+    uint8_t dplsz = radio.getDynamicPayloadSize();
+    radio.read(&buf, dplsz);
+    uint8_t msgsize = buf[0];
+    uint8_t msgcounter = buf[1];
+    uint8_t msgtype = buf[2];
 
-      uint32_t ts = millis();
-      printf("t=%08lu rxp=%i, ch=%02d, msz=%02d, ctr=%03d, mt=%02d: ",
-        ts, pipenum, chan, msgsize, msgcounter, msgtype);
-      for (uint8_t i=0; i < msgsize; i++) {
-        printf("%02X ", msgbuf[i]);
+    // read msg body
+    memcpy(msgbuf, &buf[3], msgsize);
+
+    ts = millis();
+    printf("t=%08lu, rxp=%i, ch=%02d, msgsz=%02d, ctr=%03d, mt=%02d: ",
+      ts, pipenum, chan, msgsize, msgcounter, msgtype);
+    for (uint8_t i=0; i < msgsize; i++) {
+      printf("%02X ", msgbuf[i]);
+    }
+    printf("\n");
+
+    if (msgtype == MT_NORMAL) {
+      // send ack payload in response, e.g. to change remote lights
+      //uint8_t rbuf[7] = {0x04, 1, 0x03, 0x00, 0x00, 0x03, 0x00};
+      //radio.writeAckPayload(1, &rbuf, sizeof(rbuf));
+    }
+    else if (msgtype == MT_PAIR_INIT) 
+    {
+      // received a message type for pair init
+      // a remote decides the channel and pipe to move to
+      // as a receiver, we just follow orders
+      // get channel & rx pipe addr from message
+      // also track msgctr - should not change over pairing sequence
+      remote_pair_started = true;       
+      pairing_ctr = msgcounter;
+      chan = msgbuf[0];
+      // I suspect the remote pipe address is the last 5 bytes, i.e. msgbuf[2] through [5]
+      // and msgbuf[1] is the "offset" above base address which bed receiver should listen
+      // but this "offset" always seems to be 4, in my limited testing
+      pipeaddr_offset = msgbuf[1];
+      pipeaddr[0] = msgbuf[2] + pipeaddr_offset;
+      pipeaddr[1] = msgbuf[3];
+      pipeaddr[2] = msgbuf[4];
+      pipeaddr[3] = msgbuf[5];
+      pipeaddr[4] = msgbuf[6];
+      printf("remote pair init request\n");
+    } 
+    else if (msgtype == MT_PAIR_DONE && remote_pair_started && pairing_ctr == msgcounter) 
+    {
+      // change pipe addr & chan
+      radio.openReadingPipe(1, pipeaddr);  
+      radio.setChannel(chan);        
+      printf("remote pair done, switched to chan: %02d, rxpipe: ", chan);
+      for (uint8_t i=0; i < sizeof(pipeaddr); i++) {
+        printf("%02X ", pipeaddr[i]);
       }
       printf("\n");
-
-      if (msgtype == MT_NORMAL) {
-        //uint8_t rbuf[4] = {0x05, 1, 0x01, 0x02};
-        //radio.writeAckPayload(4, &rbuf, 4);
-      }
-      else if (msgtype == MT_PAIR_INIT) 
+      remote_pair_started = false;
+      remote_pair_done = true;
+      if (pipenum == 0)
       {
-        // received a message type for pair init
-        // remote dictates channel and pipe to move to
-        // as a receiver, we just follow orders
-        // get channel & rx pipe addr from message
-        // also track msgctr - should not change over pairing sequence
-
-        printf("received pair init request\n");
-        pairing_started = true;       
-        pairing_ctr = msgcounter;
-        chan = msgbuf[0];
-        // I suspect the remote pipe address is the last 5 bytes, i.e. msgbuf[2] through [5]
-        // and msgbuf[1] is the "offset" above base address which bed receiver should listen
-        // but this "offset" always seems to be 4, in my limited test samples
-        uint8_t offset = msgbuf[1];
-        newpipe[0] = msgbuf[2] + offset;
-        newpipe[1] = msgbuf[3];
-        newpipe[2] = msgbuf[4];
-        newpipe[3] = msgbuf[5];
-        newpipe[4] = msgbuf[6];
-      } 
-      else if (msgtype == MT_PAIR_DONE && pairing_started && pairing_ctr == msgcounter) 
-      {
-        // change pipe addr & chan
-        radio.openReadingPipe(0, newpipe);
-        radio.setChannel(chan);
-        printf("switched to chan: %02d, rxpipe: ", chan);
-        for (uint8_t i=0; i < sizeof(newpipe); i++) {
-          printf("%02X ", newpipe[i]);
-        }
-        printf("\n");
-        pairing_started = false;
-        //radio.printPrettyDetails();
+        // this came from pairing address
+        // we probably re-paired with a new remote
+        // so initiate re-pair with base
+        base_pair_done = false;      
       }
-      else if (pairing_started && pairing_ctr != msgcounter)
-      {
-        // we missed pairing message sequence, reset state
-        pairing_started = false;
-        printf("pairing state reset\n");
-      }
-
+      //radio.printPrettyDetails();
     }
+    else if (remote_pair_started && pairing_ctr != msgcounter)
+    {
+      // we missed pairing message sequence, reset state
+      remote_pair_started = false;
+      printf("remote pairing state reset\n");
+    }
+  }
+
+
+  // for listen only mode, now that we are paired with remote
+  // try to emulate a remote and force a base receiver to listen
+  // on same pipeaddr & chan
+  if (listen_mode && remote_pair_done && ! base_pair_done) 
+  {
+    // first, emulate a remote, and pair with base receiver
+    // this will force base to dwell on a channel we know about
+    // so we can then listen to real remote pairing
+    Serial.println("now starting pair attempt with base receiver... ");
+    radio.setAutoAck(true);
+    while (! base_pair_done)
+    {
+      for (uint8_t i = 0; i < MAX_CHANNELS; i++)
+      {
+        if (base_pair_done)
+          break;
+        // roll through channels and broadcast pairing request
+        // until we find a base accepting our requests
+        radio.setChannel(i);
+        radio.stopListening();
+        radio.openWritingPipe(pairaddr); // 0x9669966994
+        // msg header    
+        buf[0] = 0x04; // msg body size
+        buf[1] = 0x00; // msg counter, dont care
+        buf[2] = MT_NORMAL; // msg type, normal ?
+        // msg body
+        buf[3] = 0;    // dont care
+        buf[4] = 0;
+        buf[5] = 0;
+        buf[6] = 0; 
+        // Send a dummy message. If ack'ed we know
+        // we are on the channel where base is listening
+        if (radio.write(buf, 7))
+        {
+          // msg header
+          buf[0] = 0x07; // msg body size
+          buf[1] = 0x00; // msg counter, dont care
+          buf[2] = MT_PAIR_INIT; // msg type: pair init
+          // msg body
+          buf[3] = chan; // desired channel
+          buf[4] = pipeaddr_offset; // desired base pipe addr offset
+          buf[5] = pipeaddr[0] - pipeaddr_offset;
+          buf[6] = pipeaddr[1];
+          buf[7] = pipeaddr[2];
+          buf[8] = pipeaddr[3];
+          buf[9] = pipeaddr[4];
+          printf("send pair init. ");
+          // base likes it sent twice?
+          if (radio.write(buf, 10) && radio.write(buf, 10))
+          {
+            // msg header
+            buf[0] = 0x01; // msg body size
+            buf[1] = 0x00; // msg counter, dont care
+            buf[2] = MT_PAIR_DONE; // msg type
+            // msg body
+            buf[3] = 1; // success?         
+            printf("send pair done. ");
+            if (radio.write(buf, 4))
+            {
+              printf("ack'd successfully.\n");
+              base_pair_done = true;
+            }
+            else
+            {
+              printf("NO ack from receiver.\n");
+            }
+          }
+        }
+        radio.startListening();
+      }
+      if (! base_pair_done)
+        printf("no receiver found in this pass.\n");
+      delay(1000);
+    }
+
+    // ensure we are back on remotes chan & addr
+    radio.setAutoAck(1, false); // just listen, don't ack
+    radio.setChannel(chan); // move back to remote chan
+    radio.openReadingPipe(1, pipeaddr);
+    printf("base pair done?: %d\n", base_pair_done);
+  }
 }
